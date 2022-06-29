b884fcbbbedfe3e17d841ce259cfc7f52b9d3e55
Dave Hunter <thebeakking@gmail.com>
2022-06-29 09:20:52 +0100

    remove image js

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -7,17 +7,6 @@ const images = () => {
   // We have specific configs for jpeg and png files to try
   // to really pull down asset sizes
   return src('./src/images/**/*')
-    .pipe(
-      imagemin(
-        [
-          imagemin.mozjpeg({quality: 60, progressive: true}),
-          imagemin.optipng({optimizationLevel: 5, interlaced: null})
-        ],
-        {
-          silent: true
-        }
-      )
-    )
     .pipe(dest('./dist/images'));
 };
 
